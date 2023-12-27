03c4a1969a8733aff535e814d8899f6e38da6c3e
SADAMORI KOJAKU <skojaku@binghamton.edu>
2023-12-27 15:49:31 -0500

    Initial commit

---
+++ assignments/assignment.py
@@ -0,0 +1,95 @@
+# %%
+import numpy as np
+import pandas as pd
+from typing import Any, Self
+
+
+# TODO: Implement the Perceptron class
+class Perceptron:
+    """Perceptron classifier.
+
+    Parameters
+    ------------
+    eta : float
+      Learning rate (between 0.0 and 1.0)
+    n_iter : int
+      Passes over the training dataset.
+    random_state : int
+      Random number generator seed for random weight
+      initialization.
+
+    Attributes
+    -----------
+    w_ : 1d-array
+      Weights after fitting.
+    b_ : Scalar
+      Bias unit after fitting.
+    errors_ : list
+      Number of misclassifications (updates) in each epoch.
+
+    """
+
+    def __init__(self, eta: float = 0.01, n_iter: int = 50, random_state=1) -> None:
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+        self._errors = []
+        self.w_ = None
+        self.b_ = None
+
+    def fit(self, X: np.ndarray, y: np.ndarray) -> Self:
+        """Fit training data.
+
+        Parameters
+        ----------
+        X : {array-like}, shape = [n_examples, n_features]
+          Training vectors, where n_examples is the number of examples and
+          n_features is the number of features.
+        y : array-like, shape = [n_examples]
+          Target values.
+
+        Returns
+        -------
+        self : object
+        """
+        pass
+
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """Return class label after unit step"""
+        pass
+
+
+# TODO: Implement the Adaline class
+class Adaline(Perceptron):
+    def __init__(self, eta: float, n_iter: int) -> None:
+        super(Adaline, self).__init__(eta=eta, n_iter=n_iter)
+
+    def fit(self, X: np.ndarray, y: np.ndarray) -> Self:
+        pass
+
+    def _prediction_linear(self, X: np.ndarray) -> np.ndarray:
+        pass
+
+
+# TODO: Implement the function to load the penguin data.
+def load_penguin_data():
+    """
+    This function performs the following steps:
+    1. Loads the penguin dataset from the specified URL (provided in the function).
+    2. Drops any rows with missing values using the `dropna` function from pandas.
+    3. Creates a pandas dataframe by selecting the 'bill_length_mm' and 'bill_depth_mm' columns.
+    4. Converts the selected dataframe to a numpy array, denoted as 'X'.
+    5. Slices the 'species' column from the original dataframe.
+    6. Converts the 'species' column to a numpy array, denoted as 'labels'.
+    7. Returns the numpy arrays 'X' and 'labels' in that order.
+
+    Returns
+    -------
+    tuple of numpy.ndarray
+        A tuple containing two numpy arrays. The first array, 'X', contains the bill length and depth measurements. The second array, 'labels', contains the species information.
+    """
+    url = "https://raw.githubusercontent.com/allisonhorst/palmerpenguins/c19a904462482430170bfe2c718775ddb7dbb885/inst/extdata/penguins.csv"
+    pass
+
+
+# %%
---
+++ tests/test.py
@@ -0,0 +1,63 @@
+import unittest
+import numpy as np
+import sys
+
+sys.path.append("assignments/")
+from assignment import *
+import os
+
+
+class TestPerceptron(unittest.TestCase):
+    def setUp(self):
+        self.p = Perceptron(eta=1e-2, n_iter=100)
+        # Generate 10 linearly separable points
+        self.X = np.array([[i, i + 1] for i in range(1, 11)])
+        self.y = np.array([1 if i <= 5 else 0 for i in range(1, 11)])
+
+    def test_fit(self):
+        self.p.fit(self.X, self.y)
+
+    def test_predict(self):
+        # Run fit
+        self.test_fit()
+
+        # Prediction
+        prediction = self.p.predict([[-1, -1], [20, 20]])
+        self.assertEqual(len(prediction), 2)
+        self.assertAlmostEqual(prediction[0], 1)
+        self.assertAlmostEqual(prediction[1], 0)
+
+
+class TestAdaline(unittest.TestCase):
+    def setUp(self):
+        self.p = Adaline(eta=1e-5, n_iter=1000)
+        # Generate 10 linearly separable points
+        self.X = np.array([[i, i + 1] for i in range(1, 11)])
+        self.y = np.array([1 if i <= 5 else 0 for i in range(1, 11)])
+
+    def test_fit(self):
+        self.p.fit(self.X, self.y)
+
+    def test_predict(self):
+        # Run fit
+        self.test_fit()
+
+        # Prediction
+        prediction = self.p.predict([[-1, -1], [20, 20]])
+        self.assertEqual(len(prediction), 2)
+
+
+class TestData(unittest.TestCase):
+    def test_load_penguin_data(self):
+        X, y = load_penguin_data()
+        self.assertEqual(X.shape, (333, 2))
+        self.assertEqual(y.shape, (333,))
+
+
+class TestFigure(unittest.TestCase):
+    def test_fig(self):
+        self.assertTrue(os.path.exists("./figs/decision_region.png"))
+
+
+if __name__ == "__main__":
+    unittest.main()
