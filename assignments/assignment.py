4a65e33e1049056be634d543109160e030293ba8
Sadamori Kojaku <skojaku@binghamton.edu>
2023-12-27 17:50:49 -0500

    add exercise and assignment

--- assignments/assignment.py
+++ assignments/assignment.py
@@ -4,8 +4,8 @@ import pandas as pd
 from typing import Any, Self
 
 
-# TODO: Implement the Perceptron class
-class Perceptron:
+# TODO: Implement the LogisticRegression class
+class LogisticRegression:
     """Perceptron classifier.
 
     Parameters
@@ -58,38 +58,125 @@ class Perceptron:
         """Return class label after unit step"""
         pass
 
+    def copy(self):
+        model = LogisticRegression(
+            eta=self.eta,
+            n_iter=self.n_iter,
+            random_state=self.random_state,
+        )
+        for attr in self.__dict__:
+            if isinstance(self.__dict__[attr], np.ndarray):
+                model.__dict__[attr] = self.__dict__[attr].copy()
+            else:
+                model.__dict__[attr] = self.__dict__[attr]
+        return model
 
-# TODO: Implement the Adaline class
-class Adaline(Perceptron):
-    def __init__(self, eta: float, n_iter: int) -> None:
-        super(Adaline, self).__init__(eta=eta, n_iter=n_iter)
+
+# TODO: Implement the logistic regression with L2 regularization
+class LogisticRegressionRidge(LogisticRegression):
+    """Perceptron classifier.
+
+    Parameters
+    ------------
+    eta : float
+      Learning rate (between 0.0 and 1.0)
+    n_iter : int
+      Passes over the training dataset.
+    gamma: float
+      Regularization parameter
+    random_state : int
+      Random number generator seed for random weight
+      initialization.
+
+    Attributes
+    -----------
+    w_ : 1d-array
+      Weights after fitting.
+    b_ : Scalar
+      Bias unit after fitting.
+    errors_ : list
+      Number of misclassifications (updates) in each epoch.
+
+    """
+
+    def __init__(
+        self, eta: float = 0.01, n_iter: int = 50, gamma: float = 0, random_state=1
+    ) -> None:
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+        self.gamma = gamma
+        self._errors = []
+        self.w_ = None
+        self.b_ = None
 
     def fit(self, X: np.ndarray, y: np.ndarray) -> Self:
+        """Fit training data.
+
+        Parameters
+        ----------
+        X : {array-like}, shape = [n_examples, n_features]
+          Training vectors, where n_examples is the number of examples and
+          n_features is the number of features.
+        y : array-like, shape = [n_examples]
+          Target values.
+
+        Returns
+        -------
+        self : object
+        """
         pass
 
-    def _prediction_linear(self, X: np.ndarray) -> np.ndarray:
+
+class OneVsRest:
+    """
+    OneVsRest class for multi-class classification.
+    This class uses the one-vs-rest (OvR) method for multi-class classification.
+    """
+
+    def __init__(self, classifier: LogisticRegression, n_classes: int):
+        """
+        Initialize the OneVsRest class.
+
+        Parameters
+        ----------
+        classifier : object
+            The classifier to be used for the one-vs-rest classification.
+            The classifier must have a .fit, .predict, .copy method.
+        n_classes : int
+            The number of classes in the target variable.
+        """
+        self.n_classes = n_classes
+        self.classifiers = []
+        for i in range(self.n_classes):
+            self.classifiers.append(classifier.copy())
+
+    def fit(self, X: np.ndarray, y: np.ndarray) -> Self:
+        """
+        Fit the model according to the given training data.
+
+        Parameters
+        ----------
+        X : {array-like}, shape = [n_examples, n_features]
+            Training vectors, where n_examples is the number of examples and
+            n_features is the number of features.
+        y : array-like, shape = [n_examples]
+            Target values.
+        """
         pass
 
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        Predict class labels for samples in X.
 
-# TODO: Implement the function to load the penguin data.
-def load_penguin_data():
-    """
-    This function performs the following steps:
-    1. Loads the penguin dataset from the specified URL (provided in the function).
-    2. Drops any rows with missing values using the `dropna` function from pandas.
-    3. Creates a pandas dataframe by selecting the 'bill_length_mm' and 'bill_depth_mm' columns.
-    4. Converts the selected dataframe to a numpy array, denoted as 'X'.
-    5. Slices the 'species' column from the original dataframe.
-    6. Converts the 'species' column to a numpy array, denoted as 'labels'.
-    7. Returns the numpy arrays 'X' and 'labels' in that order.
+        Parameters
+        ----------
+        X : {array-like}, shape = [n_examples, n_features]
+            Samples.
 
-    Returns
-    -------
-    tuple of numpy.ndarray
-        A tuple containing two numpy arrays. The first array, 'X', contains the bill length and depth measurements. The second array, 'labels', contains the species information.
-    """
-    url = "https://raw.githubusercontent.com/allisonhorst/palmerpenguins/c19a904462482430170bfe2c718775ddb7dbb885/inst/extdata/penguins.csv"
-    pass
-
-
-# %%
+        Returns
+        -------
+        C : array, shape = [n_examples]
+            Predicted class label per sample.
+        """
+        pass
--- assignments/utils.py
+++ assignments/utils.py
@@ -3,7 +3,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_decision_regions(X, y, classifier, resolution=0.02):
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=1000):
+    """
+    Plot decision regions of a classifier.
+
+    Parameters:
+    X : array-like, shape = [n_samples, n_features]
+        Feature Matrix.
+    y : array-like, shape = [n_samples]
+        True class labels.
+    classifier : Classifier object. Must have a .predict method.
+    test_idx : array-like, shape = [n_samples]
+        Indices of test set examples.
+    resolution : float, optional (default: 0.02)
+        The resolution of the grid for plotting decision regions.
+
+    Returns:
+    None
+    """
     # setup marker generator and color map
     markers = ("o", "s", "^", "v", "<")
     colors = ("red", "blue", "lightgreen", "gray", "cyan")
@@ -12,10 +29,14 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
     # plot the decision surface
     x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    resolution_x1 = (x1_max - x1_min) / resolution
+    resolution_x2 = (x2_max - x2_min) / resolution
     xx1, xx2 = np.meshgrid(
-        np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution)
+        np.arange(x1_min, x1_max, resolution_x1),
+        np.arange(x2_min, x2_max, resolution_x2),
     )
     lab = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    lab = np.unique(lab, return_inverse=True)[1]
     lab = lab.reshape(xx1.shape)
     plt.contourf(xx1, xx2, lab, alpha=0.3, cmap=cmap)
     plt.xlim(xx1.min(), xx1.max())
@@ -32,3 +53,27 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
             label=f"Class {cl}",
             edgecolor="black",
         )
+
+    # highlight test examples
+    if test_idx:
+        # plot all examples
+        X_test, y_test = X[test_idx, :], y[test_idx]
+
+        plt.scatter(
+            X_test[:, 0],
+            X_test[:, 1],
+            c="none",
+            edgecolor="black",
+            alpha=1.0,
+            linewidth=1,
+            marker="o",
+            s=100,
+            label="Test set",
+        )
+    plt.legend(
+        loc="upper center",
+        bbox_to_anchor=(0.5, -0.15),
+        shadow=True,
+        ncol=2,
+        frameon=False,
+    )
